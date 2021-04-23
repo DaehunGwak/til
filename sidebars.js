859e8e76e7657bc9b6730f2d3a373f36f0150a36
DaehunGwak <rhkrgns56@naver.com>
2021-04-23 20:15:30 +0900

    2021-04-23

--- sidebars.js
+++ sidebars.js
@@ -148,6 +148,7 @@ module.exports = {
             "books/cleancode/01",
             "books/cleancode/02",
             "books/cleancode/03",
+            "books/cleancode/04",
           ]
         }
       ]
